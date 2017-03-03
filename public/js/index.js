a9aa5e09093888d9203536576d113215da46dc85
ChihKang <chihkang@me.com>
2017-03-03 21:43:57 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect',function (){
   console.log('Connected to server.');
-
-  socket.emit('createMessage',{
-    from:'john',
-    text:'That is work for me'
-  });
 });
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
